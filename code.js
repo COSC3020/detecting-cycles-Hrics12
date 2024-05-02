2c3d0a36a31bc6b0ac46eb6c101686886932aba1
Hrics12 <111898293+Hrics12@users.noreply.github.com>
2024-05-02 15:29:15 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,26 @@
 function hasCycle(graph) {
+        for (let node in graph) {
+        let visitedNodes = [];
+        if (findCycle(graph, node, visitedNodes)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function findCycle(graph, node, visitedNodes) {
+    visitedNodes.push(node);
+    for (let neighbor in graph[node]) {
+        if (!visitedNodes.includes(neighbor)) {
+            let cycleFound = findCycle(graph, neighbor, visitedNodes);
+            if (cycleFound) {
+                return true;
+            } else {
+                visitedNodes = [node];
+            }
+        } else {
+            return true;
+        }
+    }
     return false;
 }
